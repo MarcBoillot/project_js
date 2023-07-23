10185d207abbcaf4d73102f53ca982af0cc3387f
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-07-23 22:26:33 +0200

    feature--upgarde

--- js/__gallery-images.js
+++ js/__gallery-images.js
@@ -1,33 +1,42 @@
-        // Fonction pour récupérer les données et les afficher
-        function fetchDataAndDisplay() {
+        // Fonction pour afficher les données et les afficher
+        function dataDisplay(data){
+            console.log(data);
+            //recuperation dans la variable tous le dom par l'id et en lien avec l'id html
             const pokemon_container = document.getElementById('dataDisplay');
-            fetch('https://pokeapi.co/api/v2/pokemon/')
+            for (const pokemon of data.results){
+                console.log(pokemon);
+                // creation de div 
+                const wrapper_pokemon = document.createElement('div');
+                const pokemon_name = document.createElement('p');
+                const pokemon_img = document.createElement('img');
+                //definition du contenu
+                pokemon_name.innerHTML = pokemon.name;
+                //ajouter les div et img au DOM
+                pokemon_container.appendChild(wrapper_pokemon);
+                wrapper_pokemon.appendChild(pokemon_name);
+                wrapper_pokemon.appendChild(pokemon_img);
+                // je recupere le pokemon concerné
+                fetch(pokemon.url)
                 .then(response => response.json())
                 .then(data => {
-                    for (const pokemon of data.results){
-                        const pokemon_name = document.createElement('p');
-                        pokemon_name.innerHTML = pokemon.name;
-                        const pokemon_img = document.createElement('img');
-                        pokemon_container.appendChild(pokemon_name);
-                        pokemon_container.appendChild(pokemon_img);
-
-                        fetch(pokemon.url)
-                        .then(response => response.json())
-                        .then(data => {
-                            pokemon_img.src = data.sprites.front_default;
-                        })
-                        
-                    }
-
-
+                    //
+                    pokemon_img.src = data.sprites.front_default;
+                    console.log(data);
                 })
+                
+            }
+        }
+        function fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => dataDisplay(data))
                 .catch(error => {
                     console.error('Une erreur s\'est produite lors de la récupération des données :', error);
                 });
         }
 
         // Appel initial pour récupérer et afficher les données au chargement de la page
-        fetchDataAndDisplay();
+        fetchData('https://pokeapi.co/api/v2/pokemon/');
 
         // Ajoutez un gestionnaire d'événement pour actualiser les données au clic sur le bouton
         const refreshButton = document.getElementById('refreshButton');
--- js/__home-posts.js
+++ js/__home-posts.js
@@ -1,28 +1,29 @@
-
-function fetchDataAndDisplay() {
+function dataDisplay(data){
     const joke_container = document.getElementById('dataDisplay');
+    for (const joke of data.jokes){
+        const wrapper_joke = document.createElement('div');
+        const joke_title = document.createElement('div');
+        joke_title.innerHTML = joke.setup;
+        const joke_reponse= document.createElement('p');
+        joke_reponse.innerHTML = joke.delivery;
+        joke_container.appendChild(wrapper_joke);
+        wrapper_joke.appendChild(joke_title);
+        wrapper_joke.appendChild(joke_reponse);
+    }
+}
+
+function fetchData() {
+    
     fetch('https://v2.jokeapi.dev/joke/Any?lang=fr&amount=10')
         .then(response => response.json())
-        .then(data => {
-            for (const joke of data.jokes){
-                console.log(joke);
-                const joke_title = document.createElement('div');
-                joke_title.innerHTML = joke.setup;
-                const joke_reponse= document.createElement('p');
-                joke_reponse.innerHTML = joke.delivery;
-                
-                joke_container.appendChild(joke_title);
-                joke_container.appendChild(joke_reponse);
-            }
-            
-        })
+        .then(data => dataDisplay(data))
         .catch(error => {
             console.error('Une erreur s\'est produite lors de la récupération des données :', error);
         });
 }
 
 // Appel initial pour récupérer et afficher les données au chargement de la page
-fetchDataAndDisplay();
+fetchData();
 // Ajoutez un gestionnaire d'événement pour actualiser les données au clic sur le bouton
 
 const refreshButton = document.getElementById('refreshButton');
