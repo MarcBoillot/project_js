6c15669ab3a19e579323b0f4010570ede671fa1f
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-07-29 17:21:29 +0200

    caroussel

--- js/__carousel-home.js
+++ js/__carousel-home.js
@@ -1,17 +1,15 @@
-class carousel {
-    constructor (element, ption = {}){
-        this.element = element
-        this.option = Object.assign({},{
-            slidesToScroll:1,
-            slideVisible:1
-        }, options)
-    }
+let slider = ['img/Capture d’écran 2023-07-28 111946.png','img/Capture d’écran 2023-07-28 190744.png','img/Capture d’écran 2023-07-28 111659.png']
+let i = 0;
+function slideClick (sens){
+   i += sens;
+   if(i<0){
+    i = slider.length-1;
+   } else if (i>=slider.length){
+    i = 0;
+   } 
+   document.getElementById('img1').src = slider[i];
 }
-document.addEventListener('DOMContentLoaded', function(){
-new carousel (document.querySelector('#carousel1'),
-{
-    slidesToScroll: 3,
-    slideVisible:3
-}) 
-
-})
+function autoSlide() {
+    slideClick(1);
+}
+setInterval(autoSlide, 3000);
